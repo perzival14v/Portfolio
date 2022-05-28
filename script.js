b94227ed3920b790fa1baa31f19523f3043db873
Javier Valdez <javier.valdez.15v@gmail.com>
2022-05-28 02:29:18 -0600

    Agregando animaciones

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const titulo = document.querySelector(".home__name")
-const subtitulo = document.querySelector(".home__description")
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    let lista = Array.from(entry.target.classList);
+    let index = lista.findIndex((element) => element == "animation") + 1;
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("slide-in", true);
-    })
-},{
-    threshold:0.4
-})
+    if (entry.isIntersecting) {
+      switch (parseInt(entry.target.classList[index])) {
+        case 1:
+          entry.target.classList.toggle("slide-left-in", true);
+          break;
+        case 2:
+          entry.target.classList.toggle("slide-rigth-in", true);
+          break;
+        case 3:
+          entry.target.classList.toggle("wype-enter", true);
+          break;
+        case 4:
+          entry.target.classList.toggle("upAndDown", true);
+          break;
+        case 5:
+          entry.target.classList.toggle("change-opacity", true);
+          break;
+        case 6:
+          entry.target.classList.toggle("zoom-in", true);
+          break;
+        default:
+          break;
+      }
+    }
+  });
+});
 
-observer.observe(subtitulo);
+function addElementsToObserve() {
+  document.querySelectorAll(".animation").forEach((element) => {
+    observer.observe(element);
+  });
+}
+
+addElementsToObserve();
